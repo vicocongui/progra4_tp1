9374ec6308243cd0cc883513a5a4a35a295eebe7
vicocongui <vicufranco2001@gmail.com>
2024-04-02 20:40:25 -0300

    modif ej 1 y 2

--- tp-uno-1.ts
+++ tp-uno-1.ts
@@ -2,7 +2,7 @@
 todas las tareas que tiene pendientes el usuario.
 O sea, que aún no están terminadas. 
 Ordenarlas por prioridad (Alta, Media, Baja).*/
-interface Persona{
+interface Persona {
     tarea: string,
     prioridad: string,
     status: string
@@ -12,17 +12,36 @@ const personas: Persona[] = [
     { tarea: "limpiar", prioridad: "baja", status: "done" },
     { tarea: "hacer progra", prioridad: "alta", status: "doing" },
     { tarea: "leer", prioridad: "media", status: "to-do" },
-    { tarea: "cocinar", prioridad: "media", status: "done" },
+    { tarea: "cocinar", prioridad: "alta", status: "doing" },
     { tarea: "coser", prioridad: "baja", status: "to-do" }
 ];
 
 function mostrarUsuario(user: Persona[]) {
-    for(let i=0; i < user.length; i++) {
+    const prioridadAlta: Persona[] = [];
+    const prioridadMedia: Persona[] = [];
+    const prioridadBaja: Persona[] = [];
+
+    for (let i = 0; i < user.length; i++) {
         const usuario = user[i];
-        if(usuario.status !== "done") {
-            console.log(`Tarea: ${usuario.tarea} Prioridad: ${usuario.prioridad} Status: ${usuario.status}`);
+        if (usuario.status !== "done") {
+            if (usuario.prioridad === "alta") {
+                prioridadAlta.push(usuario);
+            } else if (usuario.prioridad === "media") {
+                prioridadMedia.push(usuario);
+            } else {
+                prioridadBaja.push(usuario);
+            }
         }
     }
+    tareaOrdenada(prioridadAlta);
+    tareaOrdenada(prioridadMedia);
+    tareaOrdenada(prioridadBaja);
 }
 
+function tareaOrdenada(tareas: Persona[]) {
+    for (let i = 0; i < tareas.length; i++) {
+        const tarea = tareas[i];
+        console.log(`Tarea: ${tarea.tarea} Prioridad: ${tarea.prioridad} Status: ${tarea.status}`);
+    }
+}
 mostrarUsuario(personas);
